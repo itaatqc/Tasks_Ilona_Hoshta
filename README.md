6998574efcdc7815bde03d414d53c8f9a41282f9
ilona05 <ilonagoshta@gmail.com>
2015-06-18 14:32:39 +0300

    Tasks with log4j

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Tasks_Ilona_Hoshta
+# Tasks_Ilona.G
+1. Login to the yandex.ru
+2. Get today's weather
+3. Send data to your email
---
+++ tasks/src/test/java/com/softserveinc/edu/ita/pages/StartPage.java
@@ -0,0 +1,16 @@
+package com.softserveinc.edu.ita.pages;
+
+import com.softserve.edu.ita.tools.IBrowser;
+import com.softserve.edu.ita.tools.WebDriverUtils;
+
+public class StartPage extends YandexStartPage {
+	private StartPage() {
+		super();
+	}
+
+	public static StartPage load(IBrowser browser, String url) {
+		WebDriverUtils.get(browser).load(url);
+		return new StartPage();
+	}
+
+}
